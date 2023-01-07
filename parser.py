0dabe956fbea6158eef28710f0bc4aa740fab9a3
Vyacheslav314 <54808681+Vyacheslav314@users.noreply.github.com>
2023-01-07 16:06:31 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -4,8 +4,6 @@
 # # print(response.text)
 #
 # # https://openweathermap.org/
-# # 901d71a26b8430f61eb61e74f7810100
-# API_key = '4321a3d417b53045aa1b6617c529c910'
 # city_name = 'Мытищи'
 # response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&lang=ru&units=metric")
 # temp = response.json()['main']
